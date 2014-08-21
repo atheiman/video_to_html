6144b6f73863d767ee52ade642026acceca6e71e
Austin Heiman <atheimanksu@gmail.com>
2014-08-20 22:28:04 -0500

    all tests passed, need to test video playback with web server still.

--- video_to_html.sh
+++ video_to_html.sh
@@ -2,13 +2,14 @@
 
 SCRIPT_NAME=`basename $0`
 USAGE_STRING="
-Usage: $SCRIPT_NAME VIDEO_SOURCE [WEBPAGE_TITLE [HTML_SOURCE_FILE]]
+Usage: $SCRIPT_NAME VIDEO_SOURCE [OUTPUT_HTML_FILE [WEBPAGE_TITLE [HTML_SOURCE_FILE]]]
 
 Examples:
 
-	user@host:~ $ $SCRIPT_NAME the_avengers_2012.mp4
-	user@host:~ $ $SCRIPT_NAME the_avengers_2012.mp4 \"The Avengers (2014)\"
-	user@host:~ $ $SCRIPT_NAME the_avengers_2012.mp4 \"The Avengers (2014)\" my_custom_source.html
+	$SCRIPT_NAME the_avengers_2012.mp4
+	$SCRIPT_NAME the_avengers_2012.mp4 the_avengers.html
+	$SCRIPT_NAME the_avengers_2012.mp4 the_avengers.html \"The Avengers (2014)\"
+	$SCRIPT_NAME the_avengers_2012.mp4 the_avengers.html \"The Avengers (2014)\" my_custom_source.html
 
 Recommended media types: MP4, WebM, Ogg
 "
@@ -28,22 +29,23 @@ fi
 VIDEO_SOURCE=$1
 echo "Using VIDEO_SOURCE: $VIDEO_SOURCE"
 
-# Default webpage title to video source without file extension if not provided
 DEFAULT_WEBPAGE_TITLE=${VIDEO_SOURCE%%.*}
-WEBPAGE_TITLE=${2:-$DEFAULT_WEBPAGE_TITLE}
+
+# Default HTML output file
+DEFAULT_OUTPUT_HTML_FILE=$DEFAULT_WEBPAGE_TITLE.html
+OUTPUT_HTML_FILE=${2:-$DEFAULT_OUTPUT_HTML_FILE}
+echo "Using OUTPUT_HTML_FILE: $OUTPUT_HTML_FILE"
+
+# Default webpage title to video source without file extension if not provided
+WEBPAGE_TITLE=${3:-$DEFAULT_WEBPAGE_TITLE}
 echo "Using WEBPAGE_TITLE: $WEBPAGE_TITLE"
 
 # Default HTML source if not provided
 DEFAULT_HTML_SOURCE_FILE=video_html_template.html
-HTML_SOURCE_FILE=${3:-$DEFAULT_HTML_SOURCE_FILE}
+HTML_SOURCE_FILE=${4:-$DEFAULT_HTML_SOURCE_FILE}
 echo "Using HTML_SOURCE_FILE: $HTML_SOURCE_FILE"
 
-# echo "HTML_SOURCE_FILE cat output below:"
-# cat $HTML_SOURCE_FILE
-
-HTML_OUTPUT_FILE=$DEFAULT_WEBPAGE_TITLE.html
-HTML_OUTPUT_FILE=html_out.html
-echo "Using HTML_OUTPUT_FILE: $HTML_OUTPUT_FILE"
-
 # sed the new HTML file
-sed -e "s;%WEBPAGE_TITLE%;$WEBPAGE_TITLE;g" -e "s;%VIDEO_SOURCE%;$VIDEO_SOURCE;g" $HTML_SOURCE_FILE > $HTML_OUTPUT_FILE
+sed -e "s;%WEBPAGE_TITLE%;$WEBPAGE_TITLE;g" -e "s;%VIDEO_SOURCE%;$VIDEO_SOURCE;g" $HTML_SOURCE_FILE > $OUTPUT_HTML_FILE
+
+echo "Successfully created new HTML5 video file: $OUTPUT_HTML_FILE"
